332a38d5501e32b9c9e9dd53067a66e2d098e55c
Ijeoma <85398458+Ijema@users.noreply.github.com>
2022-08-19 02:50:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>My Portfolio</title>
+    <link rel="stylesheet" href="styles_for_home.css" />
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" type='text/css' >
+     
+  </head>
+  <body>
+    <!-- NAV HEAD -->
+
+    <nav id="navbar" class="nav">
+      <ul class="nav1">
+        <li>
+          <a href="#welcome-section">About</a>
+        </li>
+        <li>
+          <a href="#projects">Work</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- NAV HEAD END -->
+
+    <!--  WELCOME SECTION -->
+
+    <section id="welcome-section" class="welcome-section">
+      <h1>WELCOME TO MY PAGE. I AM ME</h1>
+      <p> Frontend/Web Developer</p>
+    </section>
+
+    <!-- WELCOME SECTION END -->
+
+    <!-- MY PROJECTS SECTION -->
+
+    <section id="projects" class="projects-section">
+      <h2 class="projects-section-header">These are some of my frontend projects</h2>
+
+      <div class="projects-grid">
+        <a href="index1.php" target="" class="project project-tile">
+          <img class="project-image" src="./images/construction-site.jpg" alt="project">
+          <p class="project-title">
+            
+            Construction Cost Management System
+            
+          </p>
+        </a>
+        <a href="https://codepen.io/freeCodeCamp/full/qRZeGZ" target="_blank" class="project project-tile">
+          <img class="project-image" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png" alt="project">
+          <p class="project-title">
+            
+            Random Quote Machine
+            
+          </p>
+        </a>
+        <a href="https://codepen.io/freeCodeCamp/full/wgGVVX" target="_blank" class="project project-tile">
+          <img class="project-image" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/calc.png" alt="project">
+          <p class="project-title">
+            
+            JavaScript Calculator
+            
+          </p>
+        </a>
+		</div>
+		<h2>These are my Web Design Projects</h2>
+		<div class="projects-grid">
+        <a href="survey_form.html" class="project project-tile">
+          <img class="project-image" src="./images/survey-image.jpg" alt="survey form">
+          <p class="project-title">
+            
+            Survey Form
+            
+          </p>
+        </a>
+        <a href="tribute_page" target="_blank" class="project project-tile">
+          <img class="project-image" src="/images/tribute-image.jpg" alt="project">
+          <p class="project-title">
+            
+            Tribute Page
+            
+          </p>
+        </a>
+        <a href="https://codepen.io/freeCodeCamp/full/KzXQgy" target="_blank" class="project project-tile">
+          <img class="project-image" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tic-tac-toe.png" alt="project">
+          <p class="project-title">
+            
+            Tic Tac Toe Game
+           
+          </p>
+        </a>
+      </div>
+
+      <a href="https://codepen.io/FreeCodeCamp/" class="btn btn-show-all" target="_blank">Show all<i class="fas fa-chevron-right"></i></a>
+    </section>
+
+    <!-- MY PROJECTS SECTION END -->
+
+    <!-- CONTACT ME SECTION -->
+
+    <section id="contact" class="contact-section">
+      <div class="contact-section-header">
+        <h2>Do you want to know more about me?</h2>
+        <p>Come let's reason together</p>
+      </div>
+      <div class="contact-links">
+        <a href="https://www.facebook.com/ijeoma.ojukwu.58" target="_blank" class="btn contact-details"><i class="fab fa-facebook-square"></i> Facebook</a>
+        <a id="profile-link" href="github.com/Ijema" target="_blank" class="btn contact-details"><i class="fab fa-github"></i> GitHub</a>
+        <a href="https://twitter.com/Ijeoma_Oj1?t=wjEmq8PaNfWcvD51EHtcjw&s=09" target="_blank" class="btn contact-details"><i class="fab fa-twitter"></i> Twitter</a>
+        <a href="ojukwuijeomaamara@gmail.com" target="_blank" class="btn contact-details"><i class="fas fa-at"></i> Send a mail</a>
+        <a href="" target="_blank" class="btn contact-details"><i class="fas fa-mobile-alt"></i> Call me</a>
+      </div>
+    </section>
+
+    <!-- CONTACT ME SECTION END -->
+
+    
+
+    <footer>
+      <!-- FOOTER SECTION --><p>
+        **Everyday is an opportunity to learn and grow.
+      </p>
+      <p>
+        © Ojukwu Ijeoma Amara
+      </p>
+    </footer>
+
+    <!-- FOOTER SECTION END -->
+  
+
+</body></html>
