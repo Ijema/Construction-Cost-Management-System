29e065ad3e10eb49d6a8a9b3773f60c89037a0aa
Ijeoma <85398458+Ijema@users.noreply.github.com>
2022-08-20 21:17:14 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 // Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
+$cleardb_host = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $clear_db = substr($cleardb_url["path"], 1);
@@ -11,4 +11,4 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 // Connect to DB
-$conn= mysqli_connect($cleardb_server, $cleardb_username,$cleardb_password,$cleardb_db)or die("Could not connect to mysql".mysqli_error($conn));
+$conn= new mysqli($cleardb_host, $cleardb_username,$cleardb_password,$cleardb_db)or die("Could not connect to mysql".mysqli_error($conn));
