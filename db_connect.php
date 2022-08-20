b70aa3c582f70b5d526867e965afb6cec716b425
Ijeoma <85398458+Ijema@users.noreply.github.com>
2022-08-20 12:51:04 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($conn));
+$conn= new mysqli('us-cdbr-east-06.cleardb.net','bcf1572b90c7c5','ff63c823','heroku_tms_db?reconnect=true')or die("Could not connect to mysql".mysqli_error($conn));
