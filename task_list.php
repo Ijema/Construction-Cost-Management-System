bd538809b5032b7e459f42c200ae561eac3c51d1
Ijeoma <85398458+Ijema@users.noreply.github.com>
2022-08-23 16:00:55 +0100

    Update task_list.php

--- task_list.php
+++ task_list.php
@@ -3,8 +3,8 @@
 	<div class="card card-outline card-success">
 		<div class="card-header">
 			<div class="card-tools">
-				<?php if($_SESSION['login_type'] == "Admin"): 
-				echo'<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="/index.php?page=manage_task"><i class="fa fa-plus"></i> Add New Task</a>';?>
+				<?php if($_SESSION['login_type'] == "Admin"): ?>
+				<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="/index1.php?page=manage_task"><i class="fa fa-plus"></i> Add New Task</a>
 				
 				<?php endif; ?>
 			</div>
