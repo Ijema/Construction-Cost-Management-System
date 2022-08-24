fd3e79072c6cf4413650511743e0ef6920767628
Ijeoma <85398458+Ijema@users.noreply.github.com>
2022-08-24 06:27:43 +0100

    Update home.php

--- home.php
+++ home.php
@@ -163,4 +163,65 @@ if($_SESSION['login_type'] != 1)
       </div>
       
 
+
+			  <!-- the DIV that will contain the widget -->
+<?php
+$cache_file = 'data.json';
+if(file_exists($cache_file)){
+  $data = json_decode(file_get_contents($cache_file));
+}else{
+  $api_url = 'https://content.api.nytimes.com/svc/weather/v2/current-and-seven-day-forecast.json';
+  $data = file_get_contents($api_url);
+  file_put_contents($cache_file, $data);
+  $data = json_decode($data);
+}
+
+$current = $data->results->current[0];
+$forecast = $data->results->seven_day_forecast;
+
+?>
+    <div class="col-12 col-sm-6 col-md-12">
+     <div class="small-box bg-light shadow-sm border">
+      <div class="inner">
+    <p>Today's Weather forcast </p>
+      <div class="single bordered" style="padding-bottom:25px;background:url('back.jpg') no-repeat ;border-top:0px;background-size: cover;">
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-12" style="font-size:20px;text-align:left;padding-left:20px;">
+            <p class="aqi-value"><?php echo convert2cen($current->temp,$current->temp_unit);?> °C</p>
+            <p class="weather-icon">
+              <img style="margin-left:-10px;" src="<?php echo $current->image;?>">
+              <?php echo $current->description;?>
+            </p>
+            <div class="weather-icon">
+              <p><strong>Wind Speed : </strong><?php echo $current->windspeed;?> <?php echo $current->windspeed_unit;?></p>
+              <p><strong>Pressue : </strong><?php echo $current->pressure;?> <?php echo $current->pressure_unit;?></p>
+              <p><strong>Visibility : </strong><?php echo $current->visibility;?> <?php echo $current->visibility_unit;?></p>
+            </div>
+          </div>
+        </div>
+		</div>
+    </div>
+  </div>
+           </div>
+          </div>
+      </div>
+        </div>
+		</div>
+      
+  <div class="container wrapper">
+
+  <div class="row">
+    <h3 class="title text-center bordered">5 Days Weather Forecast for <?php echo $current->city.' ('.$current->country.')';?></h3>
+    <?php $loop=0; foreach($forecast as $f){ $loop++;?>
+      <div class="single forecast-block bordered">
+        <h3><?php echo $f->day;?></h3>
+        <p style="font-size:1em;" class="aqi-value"><?php echo convert2cen($f->low,$f->low_unit);?> °C - <?php echo convert2cen($f->high,$f->high_unit);?> °C</p>
+        <hr style="border-bottom:1px solid #fff;">
+        <img src="<?php echo $f->image;?>">
+        <p><?php echo $f->phrase;?></p>
+      </div>
+    <?php } ?>
+  </div>
+</div>
+
              
